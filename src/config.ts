b56f96b4ddf168f6588f164816758edd95b64c94
srijancse <srijanagarwal.cse@gmail.com>
2024-02-02 16:57:56 +0530

    updates

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const CHAIN = base;
-export const CONTRACT_ADDRESS = '0x932648bd6e7e7aa04fe58636a82f62da7539abfd';
+export const CONTRACT_ADDRESS = '0x932648Bd6E7e7Aa04Fe58636a82F62da7539ABFd';
 export const TOKEN_ID = 3n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
