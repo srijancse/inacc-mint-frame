c0a22fc25e53c55c6fd83f26b420a2d660dc0be6
srijancse <srijanagarwal.cse@gmail.com>
2024-02-05 12:51:58 +0530

    005 mint updates

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const CONTRACT_ADDRESS = '0x932648Bd6E7e7Aa04Fe58636a82F62da7539ABFd';
 export const TOKEN_ID = 5n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
-  buttons: ['in-acc mint 005'],
-  image: `${SITE_URL}/og-image.jpg`,
+  buttons: ['in-acc mint 005!'],
+  image: `${SITE_URL}/opengrap-image.jpg`,
   post_url: `${SITE_URL}/api/frame`,
 });
