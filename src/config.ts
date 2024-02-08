85ac1aa4079079f3338a92bc18638fcead06836c
srijancse <srijanagarwal.cse@gmail.com>
2024-02-08 14:58:42 +0530

    007 mint

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,10 @@ export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const CHAIN = base;
 export const CONTRACT_ADDRESS = '0x932648Bd6E7e7Aa04Fe58636a82F62da7539ABFd';
-export const TOKEN_ID = 6n; // First collection is 1
+export const TOKEN_ID = 7n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
-  buttons: ['in-acc mint 006'],
+  buttons: ['in-acc mint 007'],
   image: `${SITE_URL}/opengraph-image.png`,
   post_url: `${SITE_URL}/api/frame`,
 });
